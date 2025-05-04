9490f6cf943373bfadb46107378329dac4d25728
r38k <lgmlgm6179@gmail.com>
2025-05-04 23:34:09 +0900

    Remove logo files, add deploy workflow and simplify welcome component layout

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,6 +1,6 @@
 export function Welcome({ message }: { message: string }) {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main>
       {message}
     </main>
   );
